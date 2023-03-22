020eb2ff34998e2f249c96aacecd686e08491235
Salem Kamoun <kammoun.salem@gmail.com>
2023-03-22 20:08:04 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WindowService } from './services/window.service';
+import * as auth from 'firebase/auth';
+import { AngularFireAuth } from "@angular/fire/compat/auth";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'loginByPhone';
+  windowRef:any
+  phone_number:any = "+201550426690"
+  verif_code:any
+  login1:any=true
+  login2:any=false
+  constructor(
+    public win:WindowService,
+    private angularFireAuth: AngularFireAuth,
+  ) {
+
+  }
+  ngOnInit(): void {
+    localStorage.clear()
+    this.windowRef = this.win.windowRef
+    this.windowRef.recaptchaVerifier = new auth.RecaptchaVerifier('recaptcha-container',{
+      'size': 'invisible'
+      },
+      auth.getAuth()
+    );
+    this.windowRef.recaptchaVerifier.render()
+
+  }
+
+  async signup() {
+
+
+    this.angularFireAuth.signInWithPhoneNumber( this.phone_number,this.windowRef.recaptchaVerifier).then((result:any)=>{
+      this.windowRef.confirmationResult = result;
+      localStorage.setItem("confirmation_result",JSON.stringify(result))
+      this.login1=false
+      this.login2=true
+    }).catch((error:any)=>{
+      alert("too manny connexion")
+      console.log(error);
+      
+    })
+  }
+
+  async verifyLoginCode() {
+    //this.router.navigateByUrl('/home')
+    //return
+    
+    this.windowRef.confirmationResult.confirm(this.verif_code).then( (result:any) => {
+      
+      localStorage.setItem("uid",result.user._delegate.uid)
+      localStorage.setItem("isNewUser",result.additionalUserInfo.isNewUser)
+
+      if(result.additionalUserInfo.isNewUser){
+        alert("new user")
+      } else {
+        alert("OLD user")
+      }
+        
+      
+    }).catch( (error:any) => console.log(error, "Incorrect code entered?"));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import * as firebase from 'firebase/app';
 import { AppComponent } from './app.component';
-
+firebase.initializeApp({
+  apiKey: "AIzaSyCjmitFPMgTfJDFE3b3PrHhsN8aRJGjQv8",
+  authDomain: "ramses-app-dcbaf.firebaseapp.com",
+  databaseURL: "https://ramses-app-dcbaf-default-rtdb.firebaseio.com",
+  projectId: "ramses-app-dcbaf",
+  storageBucket: "ramses-app-dcbaf.appspot.com",
+  messagingSenderId: "97452501808",
+  appId: "1:97452501808:web:598535ac61edf2d55936f5"
+})
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyCjmitFPMgTfJDFE3b3PrHhsN8aRJGjQv8",
+      authDomain: "ramses-app-dcbaf.firebaseapp.com",
+      databaseURL: "https://ramses-app-dcbaf-default-rtdb.firebaseio.com",
+      projectId: "ramses-app-dcbaf",
+      storageBucket: "ramses-app-dcbaf.appspot.com",
+      messagingSenderId: "97452501808",
+      appId: "1:97452501808:web:598535ac61edf2d55936f5"
+    }),
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
